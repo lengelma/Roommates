91068c385ea9a98171dd65a7c30ee4cdcaecbf5b
jgcvi <coatsj11@gmail.com>
2015-10-13 22:31:01 -0700

    John -- Added button listeners, no implemented classes being pointed to

--- src/com/example/roommates/DashboardActivity.java
+++ src/com/example/roommates/DashboardActivity.java
@@ -1,7 +1,11 @@
 package com.example.roommates;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class DashboardActivity extends Activity {
 
@@ -10,7 +14,47 @@ public class DashboardActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_screen);
-	    // TODO Auto-generated method stub
+        
+        // setting up the listener for grocery split button
+        Button groceryButton = (Button) findViewById(R.id.dashboard_grocery_button);
+        groceryButton.setOnClickListener(new DashboardButtonListener());
+        
+        // setting up the listener for the bill summary button
+        Button billSummaryButton = (Button) findViewById(R.id.dashboard_summary_button);
+        billSummaryButton.setOnClickListener(new DashboardButtonListener());
+        
+        // setting up the listener for the calendar button
+        Button calendarButton = (Button) findViewById(R.id.dashboard_calendar_button);
+        calendarButton.setOnClickListener(new DashboardButtonListener());
+        
+        // may need to add a photo click listener
 	}
 
+	private class DashboardButtonListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			Intent intent;
+			switch(v.getId())
+			{
+			case R.id.dashboard_calendar_button :
+				intent = new Intent();
+				break;
+				
+			case R.id.dashboard_summary_button :
+				intent = new Intent();
+				break;
+				
+			case R.id.dashboard_grocery_button :
+				intent = new Intent();
+				break;
+				
+			default :
+				return;
+			}
+			
+			startActivity(intent);
+		}
+		
+	}
 }
