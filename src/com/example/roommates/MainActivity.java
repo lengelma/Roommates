723d5a2e918da41701913188442f82ad18578c2f
jgcvi <coatsj11@gmail.com>
2015-10-13 21:14:19 -0700

    Stuff Happened

--- src/com/example/roommates/MainActivity.java
+++ src/com/example/roommates/MainActivity.java
@@ -1,17 +1,60 @@
 package com.example.roommates;
 
+import java.util.zip.Inflater;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
 
+	Button Login;
+	Button Signup;
+	EditText Email;
+	EditText Pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Login = (Button)findViewById(R.id.login);
+        Signup = (Button)findViewById(R.id.signup);
+        Email = (EditText)findViewById(R.id.email);
+        Pass = (EditText)findViewById(R.id.pass);
+        
+        Login.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if(login(Email.getText().toString(),Pass.getText().toString())){
+					Toast.makeText(getApplicationContext(), "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+				}
+				else{
+					Toast.makeText(getApplicationContext(), "LOGIN NOT SUCCESSFUL", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+        
+        Signup.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, SignupActivity.class));	
+			}
+		});
     }
 
+    public boolean login(String email, String password){
+    	
+    	if(email.contains("@") && password.length()>4){
+    		//TODO
+    		return true;
+    	}
+    	return false;
+    }
 }
